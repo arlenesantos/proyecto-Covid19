c5ee5fdc29e94d4d48e0b2e5a78d49c4e0787bf6
Eduardo A Barrios <arayabarrioseduardo@gmail.com>
2021-07-17 11:18:56 -0400

    mensaje en el modal en vez de en un alert

--- Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/data.js
+++ Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/data.js
@@ -30,42 +30,20 @@ const getCountry = async () => {
 
 //se consume la API para el gráfico por país
 const getDataModal = async (country) => { 
+    const msg = document.getElementById("msg-error");
     try {
-    const response = await fetch(`http://localhost:3000/api/countries/${country}`);
-    const { data } = await response.json(); 
-    chartCountry(data);
-    if(data.active == null || data.active == undefined){
-        alert('La información de la API a la que deseas acceder tiene un problema para mostrar los datos de este país');
-    console.log('el país que deseas ver posee problemas para mostrar su modal')
-}
-} catch (err) {
-    alert( "Nuestras disculpas, los datos tienen errores, intente solicitarlos una vez más." );
-    }};
-
-
-/*// TODO: ARREGLAR CUANDO LLEGA VACÍO EL .JSON
-//se consume la API para el gráfico por país
-const getDataModal = async (country) => { 
-    try {
-    //verificar problema en la API para países com espaço como: South Africa, una posible solución seria:     
-    //country.replace(' ', '%20'); 
-    const response = await fetch(`http://localhost:3000/api/countries/${country}`);
-    const { data } = await response.json();    
-    chartCountry(data);
-    if(country == " " || country == null || country == undefined){
-        continue getDataModal;
-        throw new Error("No hay datos para este país");
-}   else{
-    continue getDataModal;
-    return getDataModal;
-}  } catch (err) {
-  alert( "Nuestras disculpas, los datos tienen errores, intentaremos solicitarlos una vez más." );
-    alert( err.name );
-    alert( err.message );
-  }
-
+        msg.innerHTML = '';
+        const response = await fetch(`http://localhost:3000/api/countries/${country}`);
+        const { data } = await response.json(); 
+        if(data.active == null || data.active == undefined || Object.keys(data).length === 0) {
+            msg.innerHTML = 'La información de la API a la que deseas acceder tiene un problema para mostrar los datos de este país';
+            console.log('el país que deseas ver posee problemas para mostrar su modal')
+        }
+        chartCountry(data);
+    } catch (err) {
+    msg.innerHTML = "Nuestras disculpas, los datos tienen errores, intente solicitarlos una vez más.";
+    }
 };
- */
 
 export { getData, filterCountries, getCountry, getDataModal }
   

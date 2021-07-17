51bb0d26d256f588e51d96bb882ee14c6e412608
Istonehn <istonehn@gmail.com>
2021-07-16 20:58:27 -0400

    'controlando el error de la API'

--- Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/data.js
+++ Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/data.js
@@ -30,11 +30,42 @@ const getCountry = async () => {
 
 //se consume la API para el gráfico por país
 const getDataModal = async (country) => { 
+    try {
+    const response = await fetch(`http://localhost:3000/api/countries/${country}`);
+    const { data } = await response.json(); 
+    chartCountry(data);
+    if(data.active == null || data.active == undefined){
+        alert('La información de la API a la que deseas acceder tiene un problema para mostrar los datos de este país');
+    console.log('el país que deseas ver posee problemas para mostrar su modal')
+}
+} catch (err) {
+    alert( "Nuestras disculpas, los datos tienen errores, intente solicitarlos una vez más." );
+    }};
+
+
+/*// TODO: ARREGLAR CUANDO LLEGA VACÍO EL .JSON
+//se consume la API para el gráfico por país
+const getDataModal = async (country) => { 
+    try {
     //verificar problema en la API para países com espaço como: South Africa, una posible solución seria:     
-    //country.replace(' ', '%20');  
+    //country.replace(' ', '%20'); 
     const response = await fetch(`http://localhost:3000/api/countries/${country}`);
     const { data } = await response.json();    
     chartCountry(data);
+    if(country == " " || country == null || country == undefined){
+        continue getDataModal;
+        throw new Error("No hay datos para este país");
+}   else{
+    continue getDataModal;
+    return getDataModal;
+}  } catch (err) {
+  alert( "Nuestras disculpas, los datos tienen errores, intentaremos solicitarlos una vez más." );
+    alert( err.name );
+    alert( err.message );
+  }
+
 };
+ */
 
 export { getData, filterCountries, getCountry, getDataModal }
+  

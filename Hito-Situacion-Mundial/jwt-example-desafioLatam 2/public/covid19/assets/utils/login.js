dad36513e4baaa6181e6cd05793e45d4259c1cc7
Arlene Santos <arlenesantos89@gmail.com>
2021-07-18 15:12:50 -0400

    se obtiene el token y en caso de exito se actualiza el menu

--- Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/login.js
+++ Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/login.js
@@ -1,19 +1,48 @@
-/*
-//token
-const postData = async (email,password) => {
-    try{
-        const response = await fetch ("http://localhost:3000/api/login", 
-        {
-            method: 'POST',
-            body: JSON.stringify({email: email, password: password}),
-        });
-        const { token } = await response.json();
-        console.log(token)
-        localStorage.setItem('jwt-token', token); 
-        //return token;
+//se implementa la lógica para obtener el JWT cuando se ingrese el correo y contraseña a través del formulario
 
-    } catch (error){
+$('#formLogin').submit(async (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;    
+    const JWT = await postData(email, password);
+    //se cierra el modal al clicar en submit
+    $('#modalLogin').modal('hide');
+
+});
+
+//se llama a la API para obtener el token
+const postData = async (email, password) => {
+    try {
+        const response = await fetch("http://localhost:3000/api/login",
+            {
+                method: 'POST',
+                body: JSON.stringify({ email: email, password: password }),
+            });
+        const { token } = await response.json();
+        
+        //se persiste el JWT  --> **ocupar el localStorage.clear() en los proximos pasos**
+        localStorage.setItem('jwt-token', token);
+        validarToken(); 
+        return token;
+
+    } catch (error) {
         console.error(`Error: ${error}`);
     }
 };
-*/
+
+
+const validarToken = async () => {
+    const token = localStorage.getItem('jwt-token');
+    
+    //se oculta la opción del menú 'Iniciar' y se agregan las opciones 'Situación Chile' y 'Cerrar sesión'
+    if (token) {
+        document.getElementById('iniciar').classList.add('d-none');
+        document.getElementById('chile').classList.add('d-block');
+        document.getElementById('cerrar').classList.add('d-block');
+    };
+};
+validarToken();
+
+
+
+

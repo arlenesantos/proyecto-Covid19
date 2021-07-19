65ded4cc4535c434ce7854b6604e24bf69d6304a
Mario Flores <mflores14192@gmail.com>
2021-07-19 19:56:36 -0400

    modularizacion de código desarrollado por Mario

--- Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/scripts.js
+++ Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/scripts.js
@@ -1,28 +1,16 @@
-import { chartGlobal } from "./utils/graficos.js"
-import { getData, filterCountries, getCountry, getDataModal } from "./utils/data.js"
-import { showTable  } from "./utils/render.js"
-
-//se agrega un evento listener en cada link/botón para acceder al modal
-const activeBtn = (country) => {
-    country.forEach(element => {        
-        document.getElementById(`${element.location}`).addEventListener("click", () => {
-            document.querySelector(".modal-title").innerHTML = "";
-            document.querySelector(".modal-title").innerHTML = element.location;
-            getDataModal(element.location);                        
-        }); 
-    });
-}
+import {filterCountries,filterCountriesTable,getCountry} from './js/datas.js'
+import {chartGlobal} from './js/globalChart.js'
+import * as tableLogic from './js/tableCountries.js'
 
 //se aplica una IIFE para ejecutar la aplicación inmediatamente
 const init = (async () => {
-    const total = await getData();
     const country = await filterCountries();
     const local = await getCountry();
+    const dataTable = await filterCountriesTable();
 
     document.getElementById("loader").classList.add("d-none");
     document.getElementById("mainTable").classList.remove("d-none");
-    
+
     chartGlobal(local, country);
-    showTable(total);
-    activeBtn(total);    
+    tableLogic.fillTable(dataTable);
 })();

38130ad6939f2fbe5cdd5ceb5a21486957c0c007
Arlene Santos <arlenesantos89@gmail.com>
2021-07-19 22:21:09 -0400

    se resuelve las solicitaciones de revisor

--- Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/login.js
+++ Hito-Situacion-Mundial/jwt-example-desafioLatam 2/public/covid19/assets/utils/login.js
@@ -1,47 +1,54 @@
-//se implementa la lógica para obtener el JWT cuando se ingrese el correo y contraseña a través del formulario
 
-$('#formLogin').submit(async (event) => {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;    
-    const JWT = await postData(email, password);
-    //se cierra el modal al clicar en submit
-    $('#modalLogin').modal('hide');
+const login = () => {
 
-});
+    //se implementa la lógica para obtener el JWT cuando se ingrese el correo y contraseña a través del formulario
+    $('#formLogin').submit(async (event) => {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const JWT = await postData(email, password);
+        //se cierra el modal al clicar en submit
+        $('#modalLogin').modal('hide');
 
-//se llama a la API para obtener el token
-const postData = async (email, password) => {
-    try {
-        const response = await fetch("http://localhost:3000/api/login",
-            {
-                method: 'POST',
-                body: JSON.stringify({ email: email, password: password }),
-            });
-        const { token } = await response.json();
-        
-        //se persiste el JWT  --> **ocupar el localStorage.clear() en los proximos pasos**
-        localStorage.setItem('jwt-token', token);
-        validarToken(); 
-        return token;
+    });
 
-    } catch (error) {
-        console.error(`Error: ${error}`);
-    }
-};
+    //se llama a la API para obtener el token
+    const postData = async (email, password) => {
+        try {
+            const response = await fetch("http://localhost:3000/api/login",
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ email: email, password: password }),
+                });
+            const { token } = await response.json();
 
+            //se persiste el JWT  --> **ocupar el localStorage.clear() en los proximos pasos**
+            localStorage.setItem('jwt-token', token);
+            validarToken();
+            return token;
 
-const validarToken = async () => {
-    const token = localStorage.getItem('jwt-token');
-    
-    //se oculta la opción del menú 'Iniciar' y se agregan las opciones 'Situación Chile' y 'Cerrar sesión'
-    if (token) {
-        document.getElementById('iniciar').classList.add('d-none');
-        document.getElementById('chile').classList.add('d-block');
-        document.getElementById('cerrar').classList.add('d-block');
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
     };
-};
-validarToken();
+
+
+    const validarToken = async () => {
+        const token = localStorage.getItem('jwt-token');
+
+        //se oculta la opción del menú 'Iniciar' y se agregan las opciones 'Situación Chile' y 'Cerrar sesión'
+        if (token == "undefined"){
+            alert("Email o contraseña incorrectos. Intente nuevamente!");
+        } else if (token) {
+            document.getElementById('iniciar').classList.add('d-none');
+            document.getElementById('chile').classList.remove('d-none');
+            document.getElementById('cerrar').classList.remove('d-none');            
+        }
+    };
+    validarToken();
+}
+
+export { login };
 
 
 
